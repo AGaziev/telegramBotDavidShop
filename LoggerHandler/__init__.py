f485f6058682b320ab060c9393e2708f315f80ff
Alan Gaziev <agaziev@stud.kantiana.ru>
2022-03-25 16:19:42 +0300

    init composition fix commit

--- LoggerHandler/__init__.py
+++ LoggerHandler/__init__.py
@@ -1,3 +1,1 @@
-from LoggerHandler.logger import DBLogger
-from LoggerHandler.logger import AdminLogger
-from LoggerHandler.logger import InitLogger
+from LoggerHandler.logger import DBLogger, AdminLogger, InitLogger, ClientLogger
