ba8179dc35ff164baed0cb67890b7f1810e3fb8f
Alan Gaziev <agaziev@stud.kantiana.ru>
2022-04-27 02:13:02 +0300

    NO FILES ONLY JSON PARSING

--- DatabaseHandler/firebaseConfigure.py
+++ DatabaseHandler/firebaseConfigure.py
@@ -1,24 +1,16 @@
 import pyrebase
 import os
+import json
 from cryptocode import decrypt
-
-key = open('secondroomdb-bf3f10c9f6a1.json', 'w')
-key.write(decrypt(os.getenv("SAKey"), os.getenv("SAPasswordToEncrypt")))
-key.close()
 
 firebaseConfig = {
     'apiKey': os.getenv("fbApiKey"),
     'authDomain': os.getenv("fbAuthDomain"),
     'databaseURL': os.getenv("fbDatabaseURL"),
     'storageBucket': os.getenv("fbStorageBucket"),
-    'serviceAccount': os.getenv("SAKeyPath")
+    'serviceAccount': json.loads(decrypt(os.getenv("SAKey"), os.getenv("SAPasswordToEncrypt")))
 }
 
 firebase = pyrebase.initialize_app(firebaseConfig)
 db = firebase.database()
 storage = firebase.storage()
-
-# if os.path.isfile(os.getenv("SAKeyPath")):
-#     os.remove(os.getenv("SAKeyPath"))
-# else:
-#     print('FILE WITH CREDENTIALS NOT FOUND')
