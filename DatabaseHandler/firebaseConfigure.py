addf03e327ae7007ccbd6f7368743d65c44d33a2
Alan Gaziev <agaziev@stud.kantiana.ru>
2022-04-27 01:51:15 +0300

    reworked system of decrypting

--- DatabaseHandler/firebaseConfigure.py
+++ DatabaseHandler/firebaseConfigure.py
@@ -1,9 +1,9 @@
 import pyrebase
 import os
-from DatabaseHandler.serviceAccountData import getKey
+from cryptocode import decrypt
 
 with open('secondroomdb-bf3f10c9f6a1.json', 'w') as key:
-    key.write(getKey())
+    key.write(decrypt(os.getenv("SAKey"), os.getenv("SAPasswordToEncrypt")))
 
 firebaseConfig = {
     'apiKey': os.getenv("fbApiKey"),
@@ -17,7 +17,7 @@ firebase = pyrebase.initialize_app(firebaseConfig)
 db = firebase.database()
 storage = firebase.storage()
 
-# if os.path.isfile(os.getenv("SAKeyPath")):
-#     os.remove(os.getenv("SAKeyPath"))
-# else:
-#     print('FILE WITH CREDENTIALS NOT FOUND')
+if os.path.isfile(os.getenv("SAKeyPath")):
+    os.remove(os.getenv("SAKeyPath"))
+else:
+    print('FILE WITH CREDENTIALS NOT FOUND')
