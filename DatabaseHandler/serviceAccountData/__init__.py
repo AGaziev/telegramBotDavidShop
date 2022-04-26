b6141bb773d4ffe7c7de49946cb6e695a9edf903
Alan Gaziev <agaziev@stud.kantiana.ru>
2022-04-27 01:51:15 +0300

    reworked system of decrypting

--- DatabaseHandler/serviceAccountData/__init__.py
+++
@@ -1,1 +0,0 @@
-from DatabaseHandler.serviceAccountData.decryptKey import getKey
--- DatabaseHandler/serviceAccountData/decryptKey.py
+++
@@ -1,7 +0,0 @@
-from cryptocode import decrypt
-import os
-
-def getKey():
-    with open('encryptedKey','r') as key:
-        s = key.read()
-        return decrypt(s,os.getenv('SAPasswordToEncrypt'))
